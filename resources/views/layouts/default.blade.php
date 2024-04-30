a13b0c83e59716af5c21da7465182bff1d82f4a8
LanAleCoder <peter24alexander@gmail.com>
2024-04-29 23:44:25 -0600

    test de estilos y etiquetas duplicadas

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,14 +1,25 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
-@include('includes.head')
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    @vite(['resources/css/reset.css'])
+    @vite(['resources/css/layout/layout-style.css'])
+    @vite(['resources/css/utils/utils-style.css'])
+    @vite(['resources/css/header/header-style.css'])
+    @vite(['resources/css/footer/footer-style.css'])
+    {{-- Incluir nuevas meta etiquetas y la etiqueta de título --}}
+    @yield('meta-tags')
+</head>
+
 
 <body>
 
     @include('includes.header')
 
     <main class="main-content">
-        @yield('custom-content-layout')
+        @yield('custom-content-layout', 'pagina')
     </main>
 
     @include('includes.footer')
